2579f86df027ec5d04e23570f3a651469a63374b
Anna Trubnikova <trubo4ka911@gmail.com>
2022-01-31 09:19:00 +0200

    Updates

---
+++ precache-manifest.b289b1843b5b64852ec99f663e1abd2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995cbb9668d42a7e7bd5e1dba9d65291",
+    "url": "/goit-react-hw-04-hooks-feedback/index.html"
+  },
+  {
+    "revision": "be9483b8619b94d52ea8",
+    "url": "/goit-react-hw-04-hooks-feedback/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "b90fbc2b9ab96b9bf143",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.8a5eb4ca.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.8a5eb4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9483b8619b94d52ea8",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/main.5030a5c6.chunk.js"
+  },
+  {
+    "revision": "567fea8629aa48e09379",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/runtime-main.a620f54e.js"
+  }
+]);
